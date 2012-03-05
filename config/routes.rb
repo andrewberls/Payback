a615fbf179e2400ed0614be56295d123a179ee6d
Andrew Berls <andrew.berls@gmail.com>
2012-03-04 16:43:10 -0800

    Setting up pipeline configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Payback::Application.routes.draw do
+  
+  resources :users
+
+  root :to => 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
