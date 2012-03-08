a9d67bc9ee35945951d98296fa15d806b9ebb28d
Andrew Berls <andrew.berls@gmail.com>
2012-03-08 14:14:13 -0800

    Working on user tests'

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ActiveRecord::Base
 
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
 
   has_secure_password
 
-  validates_presence_of :email
-	validates_presence_of :password, :on => :create
-  validates_length_of :password, :in => 6..12  
+  #validates :name, presence: true
 
-  before_create { generate_token(:auth_token) }
+  #validates_presence_of :email
 
-  def generate_column(column)
+	#validates_presence_of :password, :on => :create
+  #validates_length_of :password, :in => 6..12  
+
+  #before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
     end while User.exists?(column => self[column])
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,49 +3,20 @@ require 'user'
 
 describe User do  
 
-	module UserSpecHelper
-	  def valid_user_attributes
-	    { :email => 'joe@bloggs.com',	      
-	      :password => 'abcdefg' }
-	  end
+	before do
+		@user = User.new(name: "Example User", email: "user@example.com")
 	end
 
-	context "A user (in general)" do
-	  include UserSpecHelper
+  subject { @user }
 
-	  setup do
-	    @user = User.new
-	  end
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 
-	  specify "should be valid with a full set of valid attributes" do
-	    @user.attributes = valid_user_attributes
-	    @user.should_be_valid
-	  end
+  it { should be_valid } # be_foo => foo?
 
-	  # specify "should be invalid without an email" do
-	  #   @user.attributes = valid_user_attributes.except(:email)
-	  #   @user.should_not_be_valid	    
-	  #   @user.email = 'joe@bloggs.com'
-	  #   @user.should_be_valid
-	  # end
-
-	  # specify "should be invalid without a password" do
-	  #   @user.attributes = valid_user_attributes.except(:password)
-	  #   @user.should_not_be_valid
-	  #   @user.password = 'abcdefg'
-	  #   @user.should_be_valid
-	  # end
-
-		# specify "should be invalid if password is not between 6 and 12 characters in length" do
-		#   @user.attributes = valid_user_attributes.except(:password)
-		#   @user.password = 'abcdefghijklm' # 13
-		#   @user.should_not_be_valid
-		#   @user.password = 'abcde' # 5
-		#   @user.should_not_be_valid
-		#   @user.password = 'abcdefg' # 7
-		#   @user.should_be_valid
-		# end
-
-	end
+  describe "when name is not present" do
+  	before { @user.name = "" }
+  	it { should_not be_valid }
+  end
 
 end
