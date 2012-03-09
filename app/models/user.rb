cb9f104e0121a86b4d7a942a9f0b4fa3291968bd
Andrew Berls <andrew.berls@gmail.com>
2012-03-08 18:33:12 -0800

    Spec and model code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  validates :name, presence: true
+  validates :name, presence: true, 
+                   length: {maximum: 50}
 
   valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,12 @@ require 'user'
 describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com", password: "12345")
+		@user = User.new(
+      name: "Example User", 
+      email: "user@example.com",
+      password: "12345",
+      password_confirmation: "12345"
+    )
 	end
 
   subject { @user }
@@ -12,6 +17,8 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   describe "when name is not present" do
   	before { @user.name = "" }
@@ -51,6 +58,16 @@ describe User do
       user_with_same_email.save
     end
 
+    it { should_not be_valid }
+  end
+
+  describe "when password is not present" do
+    before { @user.password = @user.password_confirmation = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
 
