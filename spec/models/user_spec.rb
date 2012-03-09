51939154c0dde3aaa2a8b88daa8e899f16021ebc
Andrew Berls <andrew.berls@gmail.com>
2012-03-08 18:46:25 -0800

    Authenticate specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   describe "when name is not present" do
   	before { @user.name = "" }
@@ -70,6 +71,27 @@ describe User do
   describe "when password doesn't match confirmation" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by_email(@user.email) }
+
+    describe "with valid password" do
+      it { should == found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not == user_for_invalid_password }
+      specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 4 }
+    it { should be_invalid }
   end
 
 end
