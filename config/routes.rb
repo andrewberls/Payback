26eb52ed368fd22fbc9a8e761904e077d931283e
Andrew Berls <andrew.berls@gmail.com>
2012-03-08 23:33:08 -0800

    Named routes don t work

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Payback::Application.routes.draw do
   match "login"  => "sessions#new", as: "login"
   match "logout" => "sessions#destroy", as: "logout"  
 
-  root :to => "sessions#new", as: "base"
+  root :to => "sessions#new"
 
 end
