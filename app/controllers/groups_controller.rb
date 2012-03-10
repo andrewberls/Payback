be02bd2b3968a6fe6da0847df4a2e832e593e86f
Andrew Berls <andrew.berls@gmail.com>
2012-03-10 11:50:09 -0800

    Method skeletons and auth check

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
 
+	before_filter :check_auth, :accept => [:new, :edit, :join]
+
 	def new
 	end
 
 	def create
+	end
+
+	def edit
+	end
+
+	def update
 	end
 
 	def destroy
