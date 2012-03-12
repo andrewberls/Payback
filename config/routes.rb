78752c003240c5629aca7d7a5087edb0f3655354
Andrew Berls <andrew.berls@gmail.com>
2012-03-11 20:35:03 -0700

    Final test. SSL changed routes apparently

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Payback::Application.routes.draw do
   match "login"  => "sessions#new", as: "login"
   match "logout" => "sessions#destroy", as: "logout"  
 
-  match "/" => "sessions#new"
-  #root :to => "sessions#new"
+  #match "/" => "sessions#new"
+  root :to => "sessions#new"
 
 end
