a31d1b1856b435cdb555e2fc3507956be688de8a
Andrew Berls <andrew.berls@gmail.com>
2012-03-14 17:56:21 -0700

    Working with owner assoc and spec

--- app/models/group.rb
+++ app/models/group.rb
@@ -21,9 +21,10 @@ class Group < ActiveRecord::Base
   #------------------------------
   # Users
   has_and_belongs_to_many :users
+  has_one :owner, :class_name => "User"
 
 
-  before_create :generate_gid
+  before_create [:generate_gid, :initialize_owner]
 
   def generate_gid
     # A unique external identifier
@@ -31,6 +32,15 @@ class Group < ActiveRecord::Base
     # Note that the (external) GID is distinct from the (internal) ID
     digest_string = self.id.to_s << Time.now.to_i.to_s # Combine auto id + timestamp for uniqueness
     self.gid = Digest::MD5.hexdigest(digest_string)[0..5]
+  end
+
+  def initialize_owner(user)
+    set_owner user
+    self.users << user
+  end
+
+  def set_owner(user)
+    self.owner = user
   end
 
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -9,6 +9,13 @@ describe Group do
       password: "12345",
       password_confirmation: "12345"
     )
+    @user = User.new(
+      first_name: "John",
+      last_name: "Smith",
+      email: "user@example.com",
+      password: "12345",
+      password_confirmation: "12345"
+    )
   end
 
   subject { @group }
@@ -44,8 +51,19 @@ describe Group do
   end
 
   describe "gid present before save" do
-    before { @group.save }
+    before do
+      @group.generate_gid
+      @group.save
+    end
     its(:gid) { should_not be_blank }
+  end
+
+  describe "initialize_owner" do
+    before do
+      current_user = @user
+      @group.initialize_owner(current_user)
+    end
+    #its(:owner) { should_equal @user }
   end
 
 end
