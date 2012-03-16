57e2bf62d60786288c347fd7f813f7c33b78feef
Andrew Berls <andrew.berls@gmail.com>
2012-03-16 01:00:03 -0700

    Redirect to welcome on login if no groups

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,22 +8,29 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
 
     if user && user.authenticate(params[:password])
+
       if params[:remember_me]
         cookies.permanent[:auth_token] = user.auth_token
       else
         cookies[:auth_token] = user.auth_token
-      end      
-      redirect_to expenses_path
+      end
+
+      if user.groups.blank?
+        redirect_to welcome_path
+      else
+        redirect_to expenses_path  
+      end
+
     else
       flash.now[:error] = "Invalid email or password"
       render :new
     end
+
   end
 
   def destroy    
     cookies.delete(:auth_token)
     redirect_to login_path
   end
-
 
 end
