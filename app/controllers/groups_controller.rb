390409b8167bafa7c8a85cc73939925b735d7e18
Andrew Berls <andrew.berls@gmail.com>
2012-03-17 00:11:54 -0700

    More group actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,9 @@ class GroupsController < ApplicationController
 
 	before_filter :check_auth, :accept => [:new, :edit, :join]
 
+	#------------------------------
+  # CREATE
+  #------------------------------
 	def new
 		@group = Group.new
 	end
@@ -10,35 +13,71 @@ class GroupsController < ApplicationController
 		@group = Group.new(params[:group])
 		@group.initialize_owner(current_user)
 		if @group.save
-			redirect_to expenses_path
+			redirect_to group_path(@group.gid)
 		else
 			flash.now[:error] = "Could not create your group - please check your fields and try again."
 			render :new
 		end
 	end
 
+
+	#------------------------------
+  # READ
+  #------------------------------
+  def index
+  	@groups = current_user.groups
+  end
+
+	def show
+		@group = Group.find_by_gid(params[:id])
+		if !@group
+			redirect_to groups_path
+		end
+	end
+
+
+	#------------------------------
+  # UPDATE
+  #------------------------------
 	def edit
+		@group = Group.find_by_gid(params[:id])
 	end
 
 	def update
 	end
 
+
+	#------------------------------
+  # DELETE
+  #------------------------------
 	def destroy
 	end
 
+
+	#------------------------------
+  # JOIN
+  #------------------------------
 	def join
 		# View	
 	end
 
 	def add
 		# Processing
-		@group = Group.find_by_gid(params[:gid])
-			if !@group
-				flash.now[:error] = "Invalid ID/Password combination."
+		group = Group.find_by_gid(params[:gid])
+
+		if group && group.authenticate(params[:password])
+			if group.users.include? current_user
+				flash.now[:error] = "You already belong to that group!"
 				render :join
 			else
-				# Group found - check password
+				group.users << current_user
+				flash.now[:error] = "success! ignore the error"
+				render :join
 			end
+		else
+			flash.now[:error] = "Invalid ID/Password combination."
+			render :join
+		end
 	end
 
 end
