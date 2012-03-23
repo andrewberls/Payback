9b1260a3b1f30be2dc727444e3c721c5c76905f5
Andrew Berls <andrew.berls@gmail.com>
2012-03-23 10:46:04 -0700

    Fixed single user glitch where expense was not getting duplicated

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -20,18 +20,20 @@ class ExpensesController < ApplicationController
       selected_users = group.users-[current_user] # TODO: SHOULD BE ABLE TO SELECT USERS
 
       cost_per_user = if action == :split      
-                        @expense.amount / group.users.count # Split - all group users                        
+                        @expense.amount / (selected_users+[current_user]).count # Split - selected including current                     
                       else
-                        @expense.amount / selected_users.count # Payback - all group users except the current user                        
+                        @expense.amount / selected_users.count # Payback - selected excluding current                      
                       end
 
       @expense.group    = group
       @expense.creditor = current_user
-      @expense.amount   = cost_per_user        
+      @expense.amount   = cost_per_user
 
       selected_users.each do |user|
         # TODO: Check if existing expense to this creditor already and combine?
-        user.debts << @expense
+        expense = @expense.dup
+        expense.debtor = user
+        current_user.credits << expense
       end
 
     else      
@@ -49,8 +51,8 @@ class ExpensesController < ApplicationController
   #------------------------------
   def index
     # Main dashboard
-    @credits = current_user.credits
-    @debts = current_user.debts    
+    @credits = current_user.credits.order("id DESC")
+    @debts = current_user.debts.order("id DESC")
   end
 
 
@@ -58,15 +60,9 @@ class ExpensesController < ApplicationController
   # DELETE
   #------------------------------
   def destroy
-    @expense = Expense.find_by_id(params[:id])
-
-    if @expense.destroy
-      flash[:success] = "Expense successfully removed."
-      redirect_to expenses_path
-    else
-      flash.now = "Something went wrong - please try again."
-      render :index
-    end
+    Expense.find_by_id(params[:id]).destroy
+    flash[:success] = "Expense successfully removed."
+    redirect_to expenses_path    
   end
     
 end
