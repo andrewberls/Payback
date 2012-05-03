27ee1b90a78721875456ad740fec29fe8e1c96fe
Andrew Berls <andrew.berls@gmail.com>
2012-05-03 16:04:50 -0700

    Modifying seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(
+
+admin_user = User.create(
   first_name: "Admin",
   last_name: "User",
   email: "admin@admin.com",
   password: "admin",
   password_confirmation: "admin"
 )
+
+aux_user = User.create(
+  first_name: "Joe",
+  last_name: "Schmoe",
+  email: "joe@gmail.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+seed_group = Group.create(
+  title: "Seed Group",
+  password: "password",
+  password_confirmation: "password"
+)
+
+seed_group.initialize_owner admin_user
+seed_group.users << aux_user
