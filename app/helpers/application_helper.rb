dc32836b9b76558ad8ad5ea9c937822ec20b2b4e
Andrew Berls <andrew.berls@gmail.com>
2012-06-05 15:56:10 -0700

    Current header pills

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
     time.strftime("%-d %b %Y")
   end
 
+  def action_request
+    # Returns a formatted string of the requested controller/action pair
+    # Ex "users#manage"
+    params[:controller] + "#" + params[:action]
+  end
+
 end
