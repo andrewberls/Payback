0b2b780a77acc5b65fb3f23bb4d368b6ebafd665
Andrew Berls <andrew.berls@gmail.com>
2012-06-05 15:36:29 -0700

    More generic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 admin_user = User.create(
-  first_name: "Admin",
+  first_name: "Dan",
   last_name: "User",
   email: "admin@admin.com",
   password: "admin",
@@ -15,27 +15,69 @@ admin_user = User.create(
 )
 
 aux_user_1 = User.create(
-  first_name: "Joe",
+  first_name: "Jeff",
   last_name: "Schmoe",
-  email: "joe@gmail.com",
+  email: "jeff@gmail.com",
+  password: "password",
+  password_confirmation: "password"
+)
+
+aux_user_1b = User.create(
+  first_name: "David",
+  last_name: "Dawg",
+  email: "david@gmail.com",
   password: "password",
   password_confirmation: "password"
 )
 
 aux_user_2 = User.create(
-  first_name: "Jane",
+  first_name: "Nicole",
   last_name: "Doe",
-  email: "jane@gmail.com",
+  email: "nicole@gmail.com",
   password: "password",
   password_confirmation: "password"
 )
 
 seed_group = Group.create(
-  title: "Seed Group",
+  title: "221B Baker Street",
   password: "password",
   password_confirmation: "password"
 )
 
 seed_group.initialize_owner admin_user
 seed_group.users << aux_user_1
+seed_group.users << aux_user_1b
 seed_group.users << aux_user_2
+
+# jeff owes 45 for groceries
+# nicole owes 10 for movie
+# owe 500 to david for rent
+
+groceries = Expense.create(
+  title: "Groceries",
+  amount: 45,
+  active: 1
+)
+groceries.group = seed_group
+groceries.creditor = admin_user 
+groceries.assign_to aux_user_1
+
+
+movies = Expense.create(
+  title: "Movie ticket",
+  amount: 10,
+  active: 1
+)
+movies.group = seed_group
+movies.creditor = admin_user 
+movies.assign_to aux_user_2
+
+
+rent = Expense.create(
+  title: "Textbook",
+  amount: 85,
+  active: 1
+)
+rent.group = seed_group
+rent.creditor = aux_user_1b
+rent.assign_to admin_user
