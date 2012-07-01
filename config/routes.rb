8c25f42ca1edb43e8d22b29dc8addaedf84b9264
Andrew Berls <andrew.berls@gmail.com>
2012-07-01 11:51:25 -0700

    Proper group leave

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Payback::Application.routes.draw do
   # Group actions
   match "join"  => "groups#join",  as: "join_group" # View
   match "add"   => "groups#add",   as: "add_group"  # Processing
-  match "leave" => "groups#leave", as: "leave_group"
+  match "leave/:id" => "groups#leave", as: "leave_group"
 
 
   # User actions
