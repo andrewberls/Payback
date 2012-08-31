81af5b51542f43444681b31e99091a4e4991e0ba
Andrew Berls <andrew.berls@gmail.com>
2012-08-31 13:22:01 -0700

    No more action_request! Headers are way better

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,9 @@
 module ApplicationHelper
 
-  def render_date(time)    
+  def render_date(time)
     # Returns a string timestamp
     # Ex: 7 Oct 2011
     time.strftime("%-d %b %Y")
-  end
-
-  def action_request
-    # Returns a formatted string of the requested controller/action pair
-    # Ex "users#manage"
-    params[:controller] + "#" + params[:action]
   end
 
 end
