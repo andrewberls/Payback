b9cdbe37dd11d676a1f323e2fc7229ead917ac20
Andrew Berls <andrew.berls@gmail.com>
2012-09-05 17:23:33 -0700

    Seeds structured way better. Still seeing a bit of weirdness in tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,116 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+admin_user = User.create!(full_name: "Admin User",  email: "admin@admin.com",  password: "admin",    password_confirmation: "admin")
+jeff       = User.create!(full_name: "Jeff Schmoe", email: "jeff@gmail.com",   password: "password", password_confirmation: "password")
+david      = User.create!(full_name: "David Dawg",  email: "david@gmail.com",  password: "password", password_confirmation: "password")
+nicole     = User.create!(full_name: "Nicole Doe",  email: "nicole@gmail.com", password: "password", password_confirmation: "password")
 
-admin_user = User.create!(
-  full_name: "Admin User",
-  email: "admin@admin.com",
-  password: "admin",
-  password_confirmation: "admin"
-)
-
-aux_user_1 = User.create!(
-  full_name: "Jeff Schmoe",
-  email: "jeff@gmail.com",
-  password: "password",
-  password_confirmation: "password"
-)
-
-aux_user_1b = User.create!(
-  full_name: "David Dawg",
-  email: "david@gmail.com",
-  password: "password",
-  password_confirmation: "password"
-)
-
-aux_user_2 = User.create!(
-  full_name: "Nicole Doe",
-  email: "nicole@gmail.com",
-  password: "password",
-  password_confirmation: "password"
-)
-
-seed_group = Group.create!(
-  title: "221B Baker Street",
-  password: "password",
-  password_confirmation: "password",
-  gid: 'abcdef'
-)
-
-seed_group.initialize_owner admin_user
-seed_group.users << aux_user_1
-seed_group.users << aux_user_1b
-seed_group.users << aux_user_2
-
-# nicole owes 65 for beanbag
-# jeff owes 45 for groceries
-# nicole owes 10 for movie
-# owe 500 to david for rent
-
-movies = Expense.create!(
-  title: "Movie ticket",
-  amount: 10,
-  active: 1
-)
-movies.group = seed_group
-movies.creditor = admin_user
-movies.assign_to aux_user_2
+seed_group = Group.create!(title: "221B Baker Street", password: "password", password_confirmation: "password", gid: 'abcdef') do |group|
+  group.initialize_owner(admin_user)
+  group.users << [jeff, david, nicole]
+end
 
 
-groceries = Expense.create!(
-  title: "Groceries",
-  amount: 45,
-  active: 1
-)
-groceries.group = seed_group
-groceries.creditor = admin_user
-groceries.assign_to aux_user_1
+# Nicole owes Admin 10 for movie
+Expense.create!(title: "Movie ticket", amount: 10, active: 1) do |exp|
+  exp.group    = seed_group
+  exp.creditor = admin_user
+  exp.assign_to nicole
+end
 
+# Jeff owes Admin 45 for groceries
+Expense.create!(title: "Groceries", amount: 45, active: 1) do |exp|
+  exp.group    = seed_group
+  exp.creditor = admin_user
+  exp.assign_to jeff
+end
 
-beanbag = Expense.create!(
-  title: "Beanbag chair",
-  amount: 65,
-  active: 1
-)
-beanbag.group = seed_group
-beanbag.creditor = admin_user
-beanbag.assign_to aux_user_2
+# Nicole owes Admin 65 for beanbag chair
+Expense.create!(title: "Beanbag chair", amount: 65, active: 1) do |exp|
+  exp.group    = seed_group
+  exp.creditor = admin_user
+  exp.assign_to nicole
+end
 
+# Admin owes David 85 for textbook
+Expense.create!(title: "Textbook", amount: 85, active: 1) do |exp|
+  exp.group    = seed_group
+  exp.creditor = david
+  exp.assign_to admin_user
+end
 
-rent = Expense.create!(
-  title: "Textbook",
-  amount: 85,
-  active: 1
-)
-rent.group = seed_group
-rent.creditor = aux_user_1b
-rent.assign_to admin_user
-
-
-
-
-
-blank_user_1 = User.create!(
-  full_name: "Blank One",
-  email: "blank_one@email.com",
-  password: "password",
-  password_confirmation: "password"
-)
-
-blank_user_2 = User.create!(
-  full_name: "Blank Two",
-  email: "blank_two@email.com",
-  password: "password",
-  password_confirmation: "password"
-)
-
-blank_group = Group.create!(
-  title: "Blank Group",
-  password: "blank_password",
-  password_confirmation: "blank_password",
-  gid: 'abc123'
-)
+# Users/groups with no associations
+blank_user_1 = User.create!(full_name: "Blank One", email: "blank_one@email.com", password: "password", password_confirmation: "password")
+blank_user_2 = User.create!(full_name: "Blank Two", email: "blank_two@email.com", password: "password", password_confirmation: "password")
+blank_group  = Group.create!(title: "Blank Group", password: "blank_password", password_confirmation: "blank_password", gid: 'abc123')
