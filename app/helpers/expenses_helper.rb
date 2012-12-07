e10eb20c32f0858a5327bbf2f55ea4a9191ba7ab
Andrew Berls <andrew.berls@gmail.com>
2012-12-06 16:11:40 -0800

    Factoring out mark off button

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,13 @@
 module ExpensesHelper
+
+  def mark_off_btn(expense)
+    return_path = (current_page? expenses_path) ? expenses_path : request.fullpath
+
+    link_to raw("<i class='icon-ok icon-white'></i>"),
+      { controller: 'expenses', action: 'destroy', id: expense.id, redirect: return_path },
+      :method => :delete,
+      confirm: "Mark #{number_to_currency(expense.amount)} from #{expense.debtor.first_name} completed?",
+      class: "btn no-text btn-green"
+  end
+
 end
