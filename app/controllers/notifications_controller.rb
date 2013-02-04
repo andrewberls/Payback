cd1570968f42144125006c70d568882f2a167b86
Andrew Berls <andrew.berls@gmail.com>
2013-02-04 09:39:18 -0800

    Removing notice instance var assignment tie to controller action

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,8 +9,8 @@ class NotificationsController < ApplicationController
     current_user.notifications_to.map &:mark_as_read!
 
     respond_to do |format|
-      format.html { redirect_to expenses_path }
-      format.json { render json: {} }
+      format.html { return redirect_to expenses_path }
+      format.json { return render json: {} }
     end
   end
 
