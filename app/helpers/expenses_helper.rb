388ada83c70d49e287b57e95458f9606b861cc04
Andrew Berls <andrew.berls@gmail.com>
2013-07-06 16:40:02 -0700

    Remove unecessary confirm as it's been replaced by the markoff yes/no buttons

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -31,7 +31,6 @@ module ExpensesHelper
     link_to raw("<i class='icon-ok icon-white'></i>"),
       { controller: 'expenses', action: 'destroy', id: expense.id, redirect: return_path },
       :method => :delete, :remote => true,
-      confirm: "Mark #{number_to_currency(expense.amount)} from #{expense.debtor.first_name} completed?",
       class: "btn no-text btn-green mark-off-btn"
   end
 
