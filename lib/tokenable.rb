fd61af03c8caacaae6cab4e1a8c3c16a2adb695d
Andrew Berls <andrew.berls@gmail.com>
2013-07-17 22:49:45 -0700

    Didn't actually need ActiveSupport::Concern in Tokenable - it's just a regular module

--- lib/tokenable.rb
+++ lib/tokenable.rb
@@ -1,36 +1,33 @@
-require 'active_support/concern'
+# This module is an extraction of something that is generally associated with
+# a token and expires after a period of time. For example, it's used in reset
+# tokens and group invitations.
 
 module Tokenable
-  extend ActiveSupport::Concern
+  def generate_token
+    begin
+      self.token = SecureRandom.urlsafe_base64
+    end while self.class.exists?(token: token)
+  end
 
-  included do
-    def generate_token
-      begin
-        self.token = SecureRandom.urlsafe_base64
-      end while self.class.exists?(token: token)
-    end
-
-    def expired?
-      begin
-        Time.zone.now > self.expires_at
-      rescue
-        # No expires_at field, assumed infinite
-        false
-      end
-    end
-
-    def invalid?
-      expired? || used
-    end
-
-    def mark_used
-      update_attributes(used: true)
-    end
-
-    def set_expires_at
-      lifespan = self.class.const_get(:LIFESPAN)
-      self.expires_at ||= lifespan.from_now
+  def expired?
+    begin
+      Time.zone.now > self.expires_at
+    rescue
+      # No expires_at field, assumed infinite
+      false
     end
   end
 
+  def invalid?
+    expired? || used
+  end
+
+  def mark_used
+    update_attributes(used: true)
+  end
+
+  def set_expires_at
+    lifespan = self.class.const_get(:LIFESPAN)
+    self.expires_at ||= lifespan.from_now
+  end
 end
