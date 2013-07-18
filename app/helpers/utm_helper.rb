433cecfffad78727d6f98fe6665a44e97b9e43c5
Andrew Berls <andrew.berls@gmail.com>
2013-07-17 23:34:11 -0700

    Add options hash to utm_email_link_to

--- app/helpers/utm_helper.rb
+++ app/helpers/utm_helper.rb
@@ -2,11 +2,11 @@ module UtmHelper
 
   # Appends utm query string to url, but doesn't show in name
   # TODO: this isn't very flexible, but it covers existing cases
-  def utm_email_link_to(name, url)
+  def utm_email_link_to(name, url, options={})
     utm_query = '?utm_medium=email'
     url << utm_query
     name = name.gsub(utm_query, '')
-    link_to name, url
+    link_to name, url, options
   end
 
 end
