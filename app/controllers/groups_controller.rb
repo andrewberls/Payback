87b4f411d17a8d40bea2def6f5ea03dec40114e5
Andrew Berls <andrew.berls@gmail.com>
2013-08-05 23:47:23 -0700

    Refactor groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -65,18 +65,11 @@ class GroupsController < ApplicationController
       group = Group.find_by_gid(params[:gid])
 
       if group && group.authenticate(params[:password])
-        if group.add_user(current_user)
-          flash[:success] = "You are now a member of #{group.title}!"
-          return redirect_to groups_path
-        else
-          flash.now[:error] = "You already belong to that group!"
-          return render :join
-        end
+        return add_user(group, current_user)
       else
         flash.now[:error] = "Invalid ID/Password combination."
         return render :join
       end
-
     end
   end
 
@@ -112,26 +105,13 @@ class GroupsController < ApplicationController
       # Complete automatically if already signed in, else render the form
       return complete_invitation(user: current_user, group: @group, invitation: @invitation)
     elsif request.post?
-      if @user.present?
-        # Log in
-        valid = @user.authenticate(params[:password])
-      else
-        # Sign up
-        @user = User.new(email: @invitation.recipient_email) do |u|
-          u.full_name = params[:full_name]
-          u.password  = params[:password]
-        end
-        valid = @user.save
-      end
-
-      if valid
+      if create_or_authenticate_user(params[:full_name], params[:password])
         login_user(@user)
         return complete_invitation(user: @user, group: @group, invitation: @invitation)
       else
         flash[:error] = "Error - please check your fields and try again."
         return render :invitations
       end
-
     end
   end
 
@@ -150,6 +130,20 @@ class GroupsController < ApplicationController
     @user  = User.find_by_email(@invitation.recipient_email)
   end
 
+  def create_or_authenticate_user(name, password)
+    if @user.present?
+      # Log in
+      @user.authenticate(password)
+    else
+      # Sign up
+      @user = User.new(email: @invitation.recipient_email) do |u|
+        u.full_name = name
+        u.password  = password
+      end
+      return @user.save
+    end
+  end
+
   def complete_invitation(options={})
     user  = options[:user]
     group = options[:group]
@@ -157,6 +151,16 @@ class GroupsController < ApplicationController
 
     if group.add_user(user)
       inv.mark_used
+      flash[:success] = "You are now a member of #{group.title}!"
+      return redirect_to groups_path
+    else
+      flash.now[:error] = "You already belong to that group!"
+      return render :join
+    end
+  end
+
+  def add_user(group, user)
+    if group.add_user(current_user)
       flash[:success] = "You are now a member of #{group.title}!"
       return redirect_to groups_path
     else
