f92abe6eb1169f193c6a86629f7e6ac905cd8a96
Andrew Berls <andrew.berls@gmail.com>
2013-11-02 14:09:28 -0700

    Fix pay button checkoff

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -6,7 +6,7 @@ class PaymentsController < ApplicationController
     payment_params = params[:payment]
 
     # TODO: dear god
-    @expense_ids = payment_params[:expense_ids].split(',')
+    @expense_ids = payment_params.delete(:expense_ids).split(',').map(&:to_i)
     Payment.create!(payment_params) do |payment|
       @expense_ids.each { |id| payment.expenses << Expense.find(id) }
     end
@@ -20,4 +20,5 @@ class PaymentsController < ApplicationController
     @payments_sent     = current_user.payments_sent
     @payments_received = current_user.payments_received
   end
+
 end
