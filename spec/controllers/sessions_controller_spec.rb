8163e4d0db21c88c821b9c1f85ad0b25b354af72
Andrew Berls <andrew.berls@gmail.com>
2014-02-19 16:35:36 -0800

    Session controller specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
-def post_login(user)
-  post :create, email: user.email, password: 'password'
-end
-
 describe SessionsController do
-  before do
-    cookies.delete(:auth_token)
+  let(:user) { User.make!(password: 'password') }
+
+  let(:params) do
+    {
+      email: user.email,
+      password: 'password'
+    }
   end
 
-  let(:user) { FactoryGirl.create(:user, password: 'password') }
+  before { logout! }
 
   it 'is available to all users' do
     get :new
@@ -18,33 +19,36 @@ describe SessionsController do
 
   it 'fails if any fields are blank' do
     post :create, email: user.email
+    expect(response).to render_template :new
     flash[:error].should == 'Invalid email or password'
 
     post :create, password: 'password'
+    expect(response).to render_template :new
     flash[:error].should == 'Invalid email or password'
   end
 
   it 'fails if credentials are incorrect' do
-    post :create, email: 'admin@admin.com', password: 'wrongpass'
-    expect(response).to render_template(:new)
+    post :create, email: user.email, password: 'wrongpass'
+    expect(response).to render_template :new
     flash[:error].should == 'Invalid email or password'
   end
 
 
   it 'log in user if credentials are correct' do
-    post :create, email: user.email, password: 'password'
-    response.should redirect_to new_group_path
+    post :create, params
+    expect(response).to redirect_to new_group_path
   end
 
   it 'redirects signed in users' do
-    post_login(user)
+    post :create, params
     user.auth_token.should == cookies[:auth_token]
+
     get :new
     response.should redirect_to expenses_path
   end
 
-  context 'logout' do
-    before { post_login(user) }
+  context '#destroy' do
+    before { login_user(user) }
 
     it 'logs out signed in users' do
       user.auth_token.should == cookies[:auth_token]
@@ -54,18 +58,105 @@ describe SessionsController do
     end
 
     it 'does nothing for signed out users' do
-      cookies.delete(:auth_token)
+      logout!
       get :destroy
       response.should redirect_to root_url
     end
   end
 
-  context 'forgot_password' do
-    pending
+  context '#forgot_password' do
+    let(:params) do
+      { email: user.email }
+    end
+    context 'when signed in' do
+      before { login_user(user) }
+
+      it 'renders the page' do
+        get :forgot_password
+        response.should render_template :forgot_password
+      end
+
+      it 'rejects invalid emails' do
+        post :forgot_password, email: 'doesnotexist@gmail.com'
+        response.should render_template :forgot_password
+      end
+
+      it 'rejects unauthorized emails' do
+        post :forgot_password, email: 'notmyemail@gmail.com'
+        response.should render_template :forgot_password
+      end
+
+      it 'creates a new reset token' do
+        expect { post :forgot_password, params }.to change { ResetToken.count }.by(1)
+        ResetToken.last.user.should == user
+      end
+
+      it 'redirects after success' do
+        post :forgot_password, params
+        response.should redirect_to forgot_password_path
+      end
+    end
+
+    context 'when not signed in' do
+      it 'renders the page' do
+        get :forgot_password
+        response.should render_template :forgot_password
+      end
+    end
   end
 
-  context 'reset_password' do
-    pending
+  context '#reset_password' do
+    let(:token) { ResetToken.make!(user: user) }
+
+    let(:params) do
+      {
+        token: token.token,
+        password: 'newpassword',
+        password_confirmation: 'newpassword'
+      }
+    end
+
+    it 'rejects used tokens' do
+      token.update_attributes!(used: true)
+      post :reset_password, params
+      flash[:error].should == 'Token is invalid.'
+      response.should redirect_to forgot_password_path
+    end
+
+    it 'rejects expired tokens' do
+      token.expires_at = 10.days.ago; token.save!
+      post :reset_password, params
+      flash[:error].should == 'Token is invalid.'
+      response.should redirect_to forgot_password_path
+    end
+
+    it 'rejects incorrect passwords' do
+      params[:password_confirmation] = 'does_not_match'
+      post :reset_password, params
+      response.should render_template :reset_password
+    end
+
+    it 'rejects invalid passwords' do
+      params[:password] = params[:password_confirmation] = 'x' # Too short
+      post :reset_password, params
+      response.should render_template :reset_password
+    end
+
+    it 'marks tokens as used' do
+      token.should_not be_used
+      post :reset_password, params
+      token.reload.should be_used
+    end
+
+    it 'signs in the user' do
+      post :reset_password, params
+      cookies[:auth_token].should == user.auth_token
+    end
+
+    it 'redirects after success' do
+      post :reset_password, params
+      response.should redirect_to expenses_path
+    end
   end
 
 end
