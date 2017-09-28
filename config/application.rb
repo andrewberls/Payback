7773dfa9791d02aaaa38dbf429f69983b7448a54
Andrew Berls <andrew.berls@gmail.com>
2017-09-28 17:35:47 +0200

    Mailgun install

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,15 @@ module Payback
     # Prevent initializing app and connecting to the database during precompilation
     config.assets.initialize_on_precompile = false
 
+    config.action_mailer.smtp_settings = {
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'payback.heroku.com',
+      :authentication => :plain,
+    }
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.default_url_options = {
      :host => "payback.io"
     }
